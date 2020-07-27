d794c609ff1c59b5fadd7f8f4183776d4e301538
Drew Mullen <drew@nebulaworks.com>
2020-07-27 13:53:00 -0400

    wip

--- scripts/write_comment.sh
+++ scripts/write_comment.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 
 # This script will write a comment on the github PR that calls it
-content="yo
-hello"
+content="We have created the following versions:%0A%0Aabc 123 %0A%0Abcd 123"
 
-content+="\nyo!"
-
-content="${content//'%'/'%25'}"
-content="${content//$'\n'/'%0A'}"
-content="${content//$'\r'/'%0D'}"
+# content="${content//'%'/'%25'}"
+# content="${content//$'\n'/'%0A'}"
+# content="${content//$'\r'/'%0D'}"
 echo "::set-output name=reverb::$content"
